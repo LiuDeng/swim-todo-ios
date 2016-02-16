65d67e1cdd59af869aa14668c61caca3613072c5
Ewan Mellor <ewan@swim.it>
2016-02-16 15:53:43 -0800

    Added CollectionType+Misc.
    We probably want to figure out a decent stdlib extension library and import
    that instead.

---
+++ Sources/CollectionType+Misc.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionType+Misc.swift
+//  SwimTodo
+//
+//  Created by Ewan Mellor on 2/16/16.
+//  Copyright © 2016 swim.it. All rights reserved.
+//
+
+import Foundation
+
+extension CollectionType {
+    func find(@noescape predicate: (Self.Generator.Element) throws -> Bool) rethrows -> Self.Generator.Element? {
+        return try indexOf(predicate).map({self[$0]})
+    }
+}
