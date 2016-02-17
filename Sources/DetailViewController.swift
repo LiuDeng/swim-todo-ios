7d8fa64b197147c21f103e8270dbd3186a7a3a72
Chris Sachs <chris@swim.it>
2016-02-17 10:33:07 -0800

    Update to new SwimTableViewController interface

--- Sources/DetailViewController.swift
+++ Sources/DetailViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import Recon
 import Swim
 
-class DetailViewController: SwimUITableViewController {
+class DetailViewController: SwimTableViewController {
   @IBOutlet weak var detailDescriptionLabel: UILabel!
 
   var detailItem: NodeScope? {
@@ -38,8 +38,9 @@ class DetailViewController: SwimUITableViewController {
     if detailItem == nil {
       return
     }
-    downlink = detailItem!.syncList(lane: "todo/list")
-    downlink!.keepAlive = true
+    var downlink = detailItem!.syncList(lane: "todo/list")
+    downlink.keepAlive = true
+    setDownlink(downlink, forSection: 0)
   }
 
   override func viewWillDisappear(animated: Bool) {
@@ -48,25 +49,17 @@ class DetailViewController: SwimUITableViewController {
       return
     }
 
-    detailItem!.close()
-    detailItem = nil
+    closeDownlinks()
   }
 
-  override func downlinkDidUpdate(value: Value, atIndex index: Int) {
-    let indexPath = NSIndexPath(forRow: index, inSection: downlinkSection)
-    if let cell = self.tableView.cellForRowAtIndexPath(indexPath) {
-      if value["highlight"] == Value.True {
-        cell.setHighlighted(true, animated: true)
-      } else {
-        cell.setHighlighted(false, animated: true)
-      }
-    }
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-    let value = downlink![indexPath.row]
+    let value = downlinkForSection(indexPath.section)![indexPath.row]
     cell.textLabel!.text = value["item"].text
     return cell
   }
@@ -76,16 +69,29 @@ class DetailViewController: SwimUITableViewController {
   }
 
   override func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath) {
-    if var record = downlink?[indexPath.row].record {
+    if var downlink = downlinkForSection(indexPath.section), var record = downlink[indexPath.row].record {
       record["highlight"] = Value.True
-      downlink?[indexPath.row] = Value(record)
+      downlink[indexPath.row] = Value(record)
     }
   }
 
   override func tableView(tableView: UITableView, didUnhighlightRowAtIndexPath indexPath: NSIndexPath) {
-    if var record = downlink?[indexPath.row].record {
+    if var downlink = downlinkForSection(indexPath.section), var record = downlink[indexPath.row].record {
       record.removeValueForKey("highlight")
-      downlink?[indexPath.row] = Value(record)
+      downlink[indexPath.row] = Value(record)
+    }
+  }
+
+  override func downlink(downlink: ListDownlink, didUpdate value: Value, atIndex index: Int) {
+    if let section = sectionForDownlink(downlink) {
+      let indexPath = NSIndexPath(forRow: index, inSection: section)
+      if let cell = self.tableView.cellForRowAtIndexPath(indexPath) {
+        if value["highlight"] == Value.True {
+          cell.setHighlighted(true, animated: true)
+        } else {
+          cell.setHighlighted(false, animated: true)
+        }
+      }
     }
   }
 
