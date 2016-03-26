0a8632ece21a9bc09253df8a1afe19a42bf637b3
Ewan Mellor <ewan@swim.it>
2016-03-26 13:55:16 -0700

    Tidy.

--- Sources/MasterViewController.swift
+++ Sources/MasterViewController.swift
@@ -1,92 +1,71 @@
-import Google
 import SwimSwift
 import UIKit
 
+
 class MasterViewController: UITableViewController {
-  var todo: HostScope! = nil
+    var todo: HostScope! = nil
 
-  var detailViewController: DetailViewController? = nil
-  var objects = [NodeScope]()
+    var detailViewController: DetailViewController? = nil
+    var objects = [NodeScope]()
 
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    //let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-    //self.navigationItem.rightBarButtonItem = addButton
-    if let split = self.splitViewController {
-        let controllers = split.viewControllers
-        self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        //self.navigationItem.rightBarButtonItem = addButton
+
+        if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        }
+
+        let swim = SwimClient.sharedInstance
+        let groceryList = swim.scope(node: "/todo/grocery")
+        let elementsList = swim.scope(node: "/todo/elements")
+        objects = [groceryList, elementsList]
     }
 
-    let swim = SwimClient.sharedInstance
-    let groceryList = swim.scope(node: "/todo/grocery")
-    let elementsList = swim.scope(node: "/todo/elements")
-    objects = [groceryList, elementsList]
-  }
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
 
-  override func viewWillAppear(animated: Bool) {
-    self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
-    super.viewWillAppear(animated)
-  }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+    // MARK: - Segues
 
-  //func insertNewObject(sender: AnyObject) {
-  //  objects.insert(NSDate(), atIndex: 0)
-  //  let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-  //  self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-  //}
-
-  // MARK: - Segues
-
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier == "showDetail" {
-        if let indexPath = self.tableView.indexPathForSelectedRow {
-            let object = objects[indexPath.row]
-            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-            controller.detailItem = object
-            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-            controller.navigationItem.leftItemsSupplementBackButton = true
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row]
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
         }
     }
-  }
 
-  // MARK: - Table View
+    // MARK: - Table View
 
-  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1
-  }
-
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
-  }
-
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
-    let object = objects[indexPath.row]
-    cell.textLabel!.text = object.nodeUri.path.description
-    return cell
-  }
-
-  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-  }
-
-  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-        objects.removeAtIndex(indexPath.row)
-        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-  }
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object.nodeUri.path.description
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
 }
