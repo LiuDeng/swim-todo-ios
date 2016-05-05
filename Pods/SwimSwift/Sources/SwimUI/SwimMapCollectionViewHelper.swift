d2d24325fb2f1ec39c0c94098d0d8d1052ef2bb8
Ewan Mellor <ewan@swim.it>
2016-05-04 23:27:10 -0700

    Fix install of SwimSwift 0.5.2; CocoaPods failed to copy the file correctly.

--- Pods/SwimSwift/Sources/SwimUI/SwimMapCollectionViewHelper.swift
+++ Pods/SwimSwift/Sources/SwimUI/SwimMapCollectionViewHelper.swift
@@ -27,38 +27,51 @@ public class SwimMapCollectionViewHelper: MapDownlinkDelegate {
     }
 
 
-    public func downlink(downlink: MapDownlink, didSet object: SwimModelProtocolBase, forKey key: SwimValue) {
+    public func swimMapDownlink(downlink: MapDownlink, didSet object: SwimModelProtocolBase, forKey key: SwimValue) {
         guard let objectSection = delegate?.swimObjectSection, collectionView = collectionView else {
             return
         }
 
         resortObjects()
         let index = indexOf(object)!
-        let indexPath = NSIndexPath(forRow: index, inSection: objectSection)
+        let indexPath = NSIndexPath(forItem: index, inSection: objectSection)
         collectionView.insertItemsAtIndexPaths([indexPath])
     }
 
 
-    public func downlink(downlink: MapDownlink, didRemove object: SwimModelProtocolBase, forKey key: SwimValue) {
+    public func swimMapDownlink(downlink: MapDownlink, didRemove object: SwimModelProtocolBase, forKey key: SwimValue) {
         guard let objectSection = delegate?.swimObjectSection else {
             return
         }
 
         resortObjects()
         let index = indexOf(object)!
-        let indexPath = NSIndexPath(forRow: index, inSection: objectSection)
+        let indexPath = NSIndexPath(forItem: index, inSection: objectSection)
         collectionView?.deleteItemsAtIndexPaths([indexPath])
     }
 
 
-    public func downlink(downlink: MapDownlink, didUpdate object: SwimModelProtocolBase, forKey key: SwimValue) {
+    public func swimMapDownlinkDidRemoveAll(downlink: MapDownlink, objects: [SwimValue : SwimModelProtocolBase]) {
+        guard let objectSection = delegate?.swimObjectSection else {
+            return
+        }
+
+        sortedObjects.removeAll()
+
+        let range = (0 ..< objects.count)
+        let indexPaths = range.map { NSIndexPath(forItem: $0, inSection: objectSection) }
+        collectionView?.deleteItemsAtIndexPaths(indexPaths)
+    }
+
+
+    public func swimMapDownlink(downlink: MapDownlink, didUpdate object: SwimModelProtocolBase, forKey key: SwimValue) {
         guard let objectSection = delegate?.swimObjectSection else {
             return
         }
 
         resortObjects()
         let index = indexOf(object)!
-        let indexPath = NSIndexPath(forRow: index, inSection: objectSection)
+        let indexPath = NSIndexPath(forItem: index, inSection: objectSection)
         guard let cell = collectionView?.cellForItemAtIndexPath(indexPath) else {
             return
         }
@@ -71,7 +84,7 @@ public class SwimMapCollectionViewHelper: MapDownlinkDelegate {
     }
 
 
-    private func indexOf(object: SwimModelProtocolBase) -> Int? {
+    public func indexOf(object: SwimModelProtocolBase) -> Int? {
         return sortedObjects.indexOf({ $0 === object })
     }
 
