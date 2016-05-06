6ead03f65f78053a4deaa74f22283dca6ead5b39
Ewan Mellor <ewan@swim.it>
2016-05-05 20:55:54 -0700

    Implement the handling for initials, and the serialization/deserialization for the UserPresenceModel.

--- Sources/Models/UserPresenceModel.swift
+++ Sources/Models/UserPresenceModel.swift
@@ -10,5 +10,54 @@ import SwimSwift
 
 
 class UserPresenceModel : SwimModelBase {
-    var initials : String? = nil
+    var email: String? = nil
+    var name: String? = nil
+
+    var initials: String? {
+        guard let name = name else {
+            return initialsFromEmail
+        }
+        let parts = name.componentsSeparatedByString(" ")
+        if parts.count == 0 {
+            return nil
+        }
+        else if parts.count == 1 {
+            return parts[0].firstChar
+        }
+        else {
+            return "\(parts[parts.count - 2].firstChar)\(parts.last!.firstChar)"
+        }
+    }
+
+    private var initialsFromEmail: String? {
+        guard let email = email where email != "" else {
+            return nil
+        }
+        return email.firstChar
+    }
+
+    override func swim_updateWithJSON(json: [String: AnyObject]) {
+        super.swim_updateWithJSON(json)
+
+        email = json["email"] as? String
+        name = json["name"] as? String
+    }
+
+    override func swim_toJSON() -> [String: AnyObject] {
+        var json = super.swim_toJSON()
+        if email != nil {
+            json["email"] = email
+        }
+        if name != nil {
+            json["name"] = name
+        }
+        return json
+    }
+}
+
+
+private extension String {
+    var firstChar: String {
+        return substringToIndex(startIndex.successor())
+    }
 }
