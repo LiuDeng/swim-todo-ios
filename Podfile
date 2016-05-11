ab0e967a9be9ac937fb61e6b8c5122f7a94c84a5
Ewan Mellor <ewan@swim.it>
2016-05-11 11:27:08 -0700

    Added CCHMapClusterController pod.

--- Podfile
+++ Podfile
@@ -2,6 +2,7 @@ platform :ios, '8.0'
 use_frameworks!
 
 target 'SwimTodo' do
+  pod 'CCHMapClusterController'
   pod 'Crashlytics'
   pod 'Fabric'
   pod 'Google/SignIn'
--- Pods/Target Support Files/Pods-SwimTodo/Pods-SwimTodo-frameworks.sh
+++ Pods/Target Support Files/Pods-SwimTodo/Pods-SwimTodo-frameworks.sh
@@ -85,6 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CCHMapClusterController/CCHMapClusterController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JLToast/JLToast.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReachabilitySwift/ReachabilitySwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Recon/Recon.framework"
@@ -95,6 +96,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CCHMapClusterController/CCHMapClusterController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JLToast/JLToast.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReachabilitySwift/ReachabilitySwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Recon/Recon.framework"
